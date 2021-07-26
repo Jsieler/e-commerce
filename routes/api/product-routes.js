c8d2c74139d2717c9442bdd06bd9c0ee7c1337fb
Jsieler <joshuasieler@Joshs-Air.home>
2021-07-25 20:13:49 -0600

    updated delete routes forgot product delete route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -133,6 +133,23 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(data => {
+    if (!data) {
+      res.status(404).json({ message: 'No product found with this id' });
+      return;
+    }
+    res.json(data);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
+
 
 module.exports = router;
